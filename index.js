d879fa72e5ce819c9e101db134537f8311ee6326
Jason Mulligan <jason.mulligan@avoidwork.com>
2018-06-14 17:05:01 -0400

    Removing `lib/parse.js`

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 "use strict";
 
-const path = require("path"),
-	parse = require(path.join(__dirname, "lib", "parse.js"));
+const {URL} = require("url"),
+	path = require("path"),
+	{each, isEmpty, queryString, keys} = require(path.join(__dirname, "lib", "utility.js"));
+
+function parse (arg = "", coerce = true) {
+	const uri = typeof arg === "string" ? arg : `http://${arg.headers.host || `localhost:${arg.socket.server._connectionKey.replace(/.*::/, "")}`}${arg.url}`,
+		parsed = new URL(uri),
+		result = {query: queryString(parsed, coerce)};
+
+	each(keys, i => {
+		result[i] = parsed[i];
+	});
+
+	result.auth = isEmpty(result.username) === false ? `${result.username}:${result.password}` : "";
+	result.path = `${result.pathname}${result.search}${result.hash}`;
+
+	return result;
+}
 
 module.exports = parse;
