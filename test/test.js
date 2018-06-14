0390adef7c054bfc8c89ba4b298fec8b00e6ddbd
Jason Mulligan <jason.mulligan@avoidwork.com>
2018-06-14 17:27:02 -0400

    Tweaking test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const path = require("path"),
 	parse = require(path.join(__dirname, "..", "index.js"));
 
-exports.hostless = {
+exports.pathname = {
 	setUp: function (done) {
 		this.test = function () {
 			return parse("/");
@@ -37,11 +37,27 @@ exports.schemeless = {
 
 exports.valid = {
 	setUp: function (done) {
+		this.expectation = JSON.stringify({
+			"query": {"abc": [true, false]},
+			"hash": "#something",
+			"host": "localhost:8000",
+			"hostname": "localhost",
+			"href": "http://localhost:8000/x?abc=true&abc=false#something",
+			"origin": "http://localhost:8000",
+			"password": "",
+			"pathname": "/x",
+			"port": "8000",
+			"protocol": "http:",
+			"search": "?abc=true&abc=false",
+			"username": "",
+			"auth": "",
+			"path": "/x?abc=true&abc=false#something"
+		});
 		done();
 	},
 	test: function (test) {
 		test.expect(1);
-		test.equal(JSON.stringify(parse("http://localhost:8000/x?abc=true&abc=false#something")), "{\"query\":{\"abc\":[true,false]},\"hash\":\"#something\",\"host\":\"localhost:8000\",\"hostname\":\"localhost\",\"href\":\"http://localhost:8000/x?abc=true&abc=false#something\",\"origin\":\"http://localhost:8000\",\"password\":\"\",\"pathname\":\"/x\",\"port\":\"8000\",\"protocol\":\"http:\",\"search\":\"?abc=true&abc=false\",\"username\":\"\",\"auth\":\"\",\"path\":\"/x?abc=true&abc=false#something\"}", "Output matches expectation");
+		test.equal(JSON.stringify(parse("http://localhost:8000/x?abc=true&abc=false#something")), this.expectation, "Output matches expectation");
 		test.done();
 	}
 };
